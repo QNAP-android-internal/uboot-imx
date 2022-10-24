030d9c38fea057aae1dd9078ec96a83d6c0352a0
Wig Cheng <wigcheng@ieiworld.com>
2022-10-24 17:51:30 +0800

    imx8mp-b643-ppc: add android based bootargs to header file
    Also set screen rotation to 90 degrees.

--- include/configs/imx8mp-b643-ppc_android.h
+++ include/configs/imx8mp-b643-ppc_android.h
@@ -22,6 +22,21 @@
 	"splashimage=0x50000000\0"		\
 	"fdt_high=0xffffffffffffffff\0"		\
 	"initrd_high=0xffffffffffffffff\0"	\
+	"bootargs="	\
+	"stack_depot_disable=on "	\
+	"kasan.stacktrace=off "		\
+	"console=ttymxc1,115200 "	\
+	"earlycon=ec_imx6q,0x30890000,115200 "	\
+	"init=/init "				\
+	"firmware_class.path=/vendor/firmware "	\
+	"loop.max_part=7 "			\
+	"transparent_hugepage=never "	\
+	"swiotlb=65536 "				\
+	"pci=nomsi "					\
+	"cma=1184M@0x400M-0x1000M "		\
+	"bootconfig "					\
+	"buildvariant=userdebug "		\
+	"androidboot.hwrotation=90\0"
 
 /* Enable mcu firmware flash */
 #ifdef CONFIG_FLASH_MCUFIRMWARE_SUPPORT
