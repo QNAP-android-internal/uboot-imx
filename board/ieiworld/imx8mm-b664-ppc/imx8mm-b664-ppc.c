2262f8a9a91b0aeca02cd55b8b91370dafc60ed5
Wig Cheng <wigcheng@ieiworld.com>
2023-03-02 13:52:21 +0800

    imx8mm-b664-ppc: add memory ecc setting to ft_board_setup function
    Default is 768MB size for ecc range, it's for android OS only.

--- board/ieiworld/imx8mm-b664-ppc/imx8mm-b664-ppc.c
+++ board/ieiworld/imx8mm-b664-ppc/imx8mm-b664-ppc.c
@@ -160,6 +160,25 @@ int board_init(void)
 	return 0;
 }
 
+#ifdef CONFIG_OF_BOARD_SETUP
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+#ifdef CONFIG_ANDROID_SUPPORT
+	int rc;
+	phys_addr_t ecc0_start = 0xc0000000;
+	size_t ecc_size = 0x30000000;
+
+	rc = add_res_mem_dt_node(blob, "ecc", ecc0_start, ecc_size);
+	if (rc < 0) {
+		printf("Could not create ecc0 reserved-memory node.\n");
+		return rc;
+	}
+#endif
+
+	return 0;
+}
+#endif
+
 int board_late_init(void)
 {
 #ifdef CONFIG_ENV_IS_IN_MMC
