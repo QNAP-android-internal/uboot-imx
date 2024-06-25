f91997d681af2c8279fed4414d8f57ad4eed60b6
lcwang <lcwang@ieiworld.com>
2024-06-25 16:26:17 +0800

    board: ieiworld: imx8mp-b643-ppc.c: fix merge errors from 2023.04 to 2024.04 version
    copy the content from imx8mp_evk.c and make modifications
    by referrig to the following commits
    
    merge list:
    
    5d4cd1c420 imx8mp-b643-ppc: remove usb tcpc based relate function from board file

--- board/ieiworld/imx8mp-b643-ppc/imx8mp-b643-ppc.c
+++ board/ieiworld/imx8mp-b643-ppc/imx8mp-b643-ppc.c
@@ -3,7 +3,6 @@
  * Copyright 2019 NXP
  */
 
-#include <common.h>
 #include <efi_loader.h>
 #include <env.h>
 #include <errno.h>
@@ -61,10 +60,10 @@ struct efi_fw_image fw_images[] = {
 
 struct efi_capsule_update_info update_info = {
 	.dfu_string = "mmc 2=flash-bin raw 0 0x2000 mmcpart 1",
+	.num_images = ARRAY_SIZE(fw_images),
 	.images = fw_images,
 };
 
-u8 num_image_type_guids = ARRAY_SIZE(fw_images);
 #endif /* EFI_HAVE_CAPSULE_SUPPORT */
 
 int board_early_init_f(void)
@@ -162,9 +161,9 @@ static struct dwc3_device dwc3_device_data = {
 	.power_down_scale = 2,
 };
 
-int usb_gadget_handle_interrupts(int index)
+int dm_usb_gadget_handle_interrupts(struct udevice *dev)
 {
-	dwc3_uboot_handle_interrupt(index);
+	dwc3_uboot_handle_interrupt(dev);
 	return 0;
 }
 
@@ -233,48 +232,50 @@ int board_usb_cleanup(int index, enum usb_init_type init)
 
 	return ret;
 }
+
 #endif
 
 static void setup_fec(void)
 {
-	struct iomuxc_gpr_base_regs *gpr =
-		(struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
+    struct iomuxc_gpr_base_regs *gpr =
+        (struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
 
-	/* Enable RGMII TX clk output */
-	setbits_le32(&gpr->gpr[1], BIT(22));
+    /* Enable RGMII TX clk output */
+    setbits_le32(&gpr->gpr[1], BIT(22));
 }
 
 static int setup_eqos(void)
 {
-	struct iomuxc_gpr_base_regs *gpr =
-		(struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
+    struct iomuxc_gpr_base_regs *gpr =
+        (struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
 
-	/* set INTF as RGMII, enable RGMII TXC clock */
-	clrsetbits_le32(&gpr->gpr[1],
-			IOMUXC_GPR_GPR1_GPR_ENET_QOS_INTF_SEL_MASK, BIT(16));
-	setbits_le32(&gpr->gpr[1], BIT(19) | BIT(21));
+    /* set INTF as RGMII, enable RGMII TXC clock */
+    clrsetbits_le32(&gpr->gpr[1],
+            IOMUXC_GPR_GPR1_GPR_ENET_QOS_INTF_SEL_MASK, BIT(16));
+    setbits_le32(&gpr->gpr[1], BIT(19) | BIT(21));
 
-	return set_clk_eqos(ENET_125MHZ);
+    return set_clk_eqos(ENET_125MHZ);
 }
 
 #if CONFIG_IS_ENABLED(NET)
 int board_phy_config(struct phy_device *phydev)
 {
-	if (phydev->drv->config)
-		phydev->drv->config(phydev);
-	return 0;
+    if (phydev->drv->config)
+        phydev->drv->config(phydev);
+    return 0;
 }
 #endif
 
+
 int board_init(void)
 {
-	if (IS_ENABLED(CONFIG_FEC_MXC)) {
-		setup_fec();
-	}
+    if (IS_ENABLED(CONFIG_FEC_MXC)) {
+        setup_fec();
+    }
 
-	if (IS_ENABLED(CONFIG_DWC_ETH_QOS)) {
-		setup_eqos();
-	}
+    if (IS_ENABLED(CONFIG_DWC_ETH_QOS)) {
+        setup_eqos();
+    }
 
 #ifdef CONFIG_NAND_MXS
 	setup_gpmi_nand();
@@ -308,7 +309,7 @@ bool is_power_key_pressed(void) {
 
 #ifdef CONFIG_SPL_MMC
 #define UBOOT_RAW_SECTOR_OFFSET 0x40
-unsigned long spl_mmc_get_uboot_raw_sector(struct mmc *mmc)
+unsigned long spl_mmc_get_uboot_raw_sector(struct mmc *mmc, unsigned long raw_sect)
 {
 	u32 boot_dev = spl_boot_device();
 	switch (boot_dev) {
