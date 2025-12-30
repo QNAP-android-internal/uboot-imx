b29dd9f64668a383e295bdac0ed7624a6b84cca9
Jason Yang <jasonyang@ieiworld.com>
2025-12-30 15:24:11 +0800

    board: ieiworld: common: fix merge errors from 2024.04 to 2025.04 version
    copy the files from board/freescale/common
    
    Change-Id: I693ffefa2c79a5b1d0858e388b2b5e9a8155d9bb

--- board/ieiworld/common/epdc_setup.c
+++ board/ieiworld/common/epdc_setup.c
@@ -4,7 +4,6 @@
  *
  * Peng Fan <Peng.Fan@freescale.com>
  */
-#include <common.h>
 #include <linux/err.h>
 #include <linux/types.h>
 #include <malloc.h>
@@ -13,6 +12,7 @@
 #include <cpu_func.h>
 #include <env.h>
 #include <asm/cache.h>
+#include <vsprintf.h>
 
 #define is_digit(c)	((c) >= '0' && (c) <= '9')
 __weak int mmc_get_env_devno(void)
