c3a690793c8255e80e947cc84b42c6d34a7525f9
Jason Yang <jasonyang@ieiworld.com>
2026-03-23 14:13:42 +0800

    install_uboot_imx8.sh: change SM config to smarc-imx95
    Change-Id: I91f8f9a46bb95c9b65a239a9c865d727dbb92ba0

--- install_uboot_imx8.sh
+++ install_uboot_imx8.sh
@@ -32,7 +32,7 @@ ATF_BOOT_UART_BASE="0x30890000"
 # Config for i.mx95
 IMX_SM_GIT_REPO="https://github.com/QNAP-android-internal/imx-sm.git"
 IMX_SM_BRANCH_VER="iei-imx_6.12.34_2.1.0"
-IMX_SM_CONFIG="mx95evk"
+IMX_SM_CONFIG="smarc-imx95"
 IMX_OEI_GIT_REPO="https://github.com/QNAP-android-internal/imx-oei.git"
 IMX_OEI_BRANCH_VER="iei-imx_6.12.34_2.1.0"
 IMX_OEI_CONFIG="smarc-imx95"
